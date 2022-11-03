041c9ac88e76dcf3bd6643a8305d3a72d42391fe
Myrrdin <stephan.jeanba@gmail.com>
2022-11-03 11:29:30 +0100

    [🚧 WIP](Back): mise a jour des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Tricks;
+use App\Entity\Video;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\String\Slugger\AsciiSlugger;
@@ -33,15 +34,60 @@ class AppFixtures extends Fixture
         $c4->setLabel('Switch');
         $manager->persist($c4);
 
-        // $product = new Product();
-        // $manager->persist($product);
+        // Image Fixture
+
+        // Video Fixture
+        $v1 = new Video();
+        $v1->setUrl('https://youtu.be/LZqKaZyby2g');
+        $manager->persist($v1);
+
+        $v2 = new Video();
+        $v2->setUrl('https://youtu.be/jH76540wSqU');
+        $manager->persist($v2);
+
+        $v3 = new Video();
+        $v3->setUrl('https://youtu.be/_CN_yyEn78M');
+        $manager->persist($v3);
+
+        $v4 = new Video();
+        $v4->setUrl('https://youtu.be/BJYwC5wOt4o');
+        $manager->persist($v4);
+
+        $v5 = new Video();
+        $v5->setUrl('https://youtu.be/LWUfrwCofuA');
+        $manager->persist($v5);
+
+        $v6 = new Video();
+        $v6->setUrl('https://youtu.be/LWUfrwCofuA');
+        $manager->persist($v6);
+
+        $v7 = new Video();
+        $v7->setUrl('https://youtu.be/M-W7Pmo-YMY');
+        $manager->persist($v7);
+
+        $v8 = new Video();
+        $v8->setUrl('https://youtu.be/4vGEOYNGi_c');
+        $manager->persist($v8);
+
+        $v9 = new Video();
+        $v9->setUrl('https://youtu.be/f9FjhCt_w2U');
+        $manager->persist($v9);
+
+        $v10 = new Video();
+        $v10->setUrl('https://youtu.be/KEdFwJ4SWq4');
+        $manager->persist($v10);
+
+        $v11 = new Video();
+        $v11->setUrl('https://youtu.be/6yA3XqjTh_w');
+        $manager->persist($v11);
+
         $t1 = new Tricks();
         $t1->setDescription('Cette figure consiste à saisi l\'avant de la planche, avec la main avant, du côté de la carre frontside')
             ->setName('Japan Air')
             ->setSlug($slugger->slug('Japan Air'))
             ->setCategory($c1)
+            ->addVideo($v1)
             ->setCreatedAt($now);
-
         $manager->persist($t1);
 
         $t2 = new Tricks();
@@ -49,6 +95,7 @@ class AppFixtures extends Fixture
             ->setName('Backside Air')
             ->setSlug($slugger->slug("Backside Air"))
             ->setCategory($c2)
+            ->addVideo($v2)
             ->setCreatedAt($now);
         $manager->persist($t2);
 
@@ -57,6 +104,7 @@ class AppFixtures extends Fixture
             ->setName('Switch Backside Rodeo 720')
             ->setSlug($slugger->slug('Switch Backside Rodeo 720'))
             ->setCategory($c2)
+            ->addVideo($v3)
             ->setCreatedAt($now);
 
         $manager->persist($t3);
@@ -66,6 +114,7 @@ class AppFixtures extends Fixture
             ->setName('One Foot Tricks')
             ->setSlug($slugger->slug('One Foot Tricks'))
             ->setCategory($c3)
+            ->addVideo($v4)
             ->setCreatedAt($now);
         $manager->persist($t4);
 
@@ -74,9 +123,73 @@ class AppFixtures extends Fixture
             ->setName('Switch Method')
             ->setSlug($slugger->slug('Switch Method'))
             ->setCategory($c4)
+            ->addVideo($v5)
             ->setCreatedAt($now);
 
         $manager->persist($t5);
+
+        $t6 = new Tricks();
+        $t6->setDescription('saisie de la partie arrière de la planche, avec la main arrière.')
+            ->setName('Tail grab')
+            ->setSlug($slugger->slug('Tail grab'))
+            ->setCategory($c4)
+            ->addVideo($v6)
+            ->setCreatedAt($now);
+
+        $manager->persist($t6);
+
+        $t7 = new Tricks();
+        $t7->setDescription('saisie de la partie avant de la planche, avec la main avant.')
+            ->setName('Nose grab')
+            ->setSlug($slugger->slug('Nose grab'))
+            ->setCategory($c4)
+            ->addVideo($v7)
+            ->setCreatedAt($now);
+
+        $manager->persist($t7);
+
+        $t8 = new Tricks();
+        $t8->setDescription('saisie du carre frontside à l\'arrière avec la main avant ')
+            ->setName('Seat belt')
+            ->setSlug($slugger->slug('Seat belt'))
+            ->setCategory($c4)
+            ->addVideo($v8)
+            ->setCreatedAt($now);
+
+        $manager->persist($t8);
+
+
+
+        $t9 = new Tricks();
+        $t9->setDescription('saisie de la carre backside de la planche entre les deux pieds avec la main arrière.')
+            ->setName('Stalefish')
+            ->setSlug($slugger->slug('Stalefish'))
+            ->setCategory($c4)
+            ->addVideo($v9)
+            ->setCreatedAt($now);
+
+        $manager->persist($t9);
+
+
+        $t10 = new Tricks();
+        $t10->setDescription('saisie de la carre backside de la planche, entre les deux pieds, avec la main avant.')
+            ->setName('Melancholie')
+            ->setSlug($slugger->slug('Melancholie'))
+            ->setCategory($c4)
+            ->addVideo($v10)
+            ->setCreatedAt($now);
+
+        $manager->persist($t10);
+
+        $t11 = new Tricks();
+        $t11->setDescription('aisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière.')
+            ->setName('Indy')
+            ->setSlug($slugger->slug('Indy'))
+            ->setCategory($c4)
+            ->addVideo($v11)
+            ->setCreatedAt($now);
+
+        $manager->persist($t11);
 
         $manager->flush();
     }
