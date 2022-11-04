e0c3a2f25428cf8706b529bc601bd00ae6b11b52
Myrrdin <stephan.jeanba@gmail.com>
2022-11-04 18:44:04 +0100

    [💚 chores](*):

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('handleVideo', './assets/handleVideo.js')
     .addEntry('slider', './assets/handleSlider.js')
+    .addEntry('handleDelete', './assets/deletePicture.js')
     // .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
