af8213819971e3964fee0069b6a33c407fce746f
Myrrdin <stephan.jeanba@gmail.com>
2022-11-04 11:34:34 +0100

    [🚧 WIP](Front): misa a jour sliders

--- assets/handleSlider.js
+++ assets/handleSlider.js
@@ -1,3 +1,5 @@
 import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
 
-new Glide('.glide').mount({ Controls, Breakpoints });
+new Glide('.glideImage').mount({ Controls, Breakpoints });
+new Glide('.glideVideo').mount({ Controls, Breakpoints });
+
